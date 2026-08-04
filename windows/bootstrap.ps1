7dce982c0dfe12c43aa12430bda1b0831dd7983d
C4 Patino <c4patino@gmail.com>
2026-08-03 23:55:34 -0500

    feat: add windows bootstrap script to dotfiles

---
+++ windows/bootstrap.ps1
@@ -0,0 +1,9 @@
+winget install -e --scope user --id ajeetdsouza.zoxide
+winget install -e --scope user --id direnv.direnv
+winget install -e --scope user --id rsteube.Carapace
+winget install -e --scope user --id Nushell.Nushell
+winget install -e --scope user --id junegunn.fzf
+winget install -e --scope user --id Starship.Starship
+winget install -e --scope user --id=JesseDuffield.lazygit
+winget install -e --id Microsoft.PowerToys
+winget install -e --id Microsoft.PowerShell
